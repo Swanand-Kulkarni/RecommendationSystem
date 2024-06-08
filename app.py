9da1bd4b1ee3f2406026cd5c4dc4eae934b7528f
Swanand-Kulkarni <swanandk2000@gmail.com>
2024-06-08 21:15:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,123 @@
+import streamlit as st 
+import streamlit.components.v1 as stc 
+
+
+import pandas as pd 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
+
+
+# Load Our Dataset
+def load_data(data):
+	df = pd.read_csv(data)
+	return df 
+
+
+#Vectorize 
+#Cosine Similarity Matrix
+
+def vectorize_text_to_cosine_mat(data):
+	count_vect = CountVectorizer()
+	cv_mat = count_vect.fit_transform(data)
+	
+	cosine_sim_mat = cosine_similarity(cv_mat)
+	return cosine_sim_mat
+
+# Recommendation System
+@st.cache
+def get_recommendation(title,cosine_sim_mat,df,num_of_rec=10):
+	
+	course_indices = pd.Series(df.index,index=df['course_title']).drop_duplicates(keep='first')
+	
+	idx = course_indices[title]
+
+	
+	sim_scores =list(enumerate(cosine_sim_mat[idx]))
+	sim_scores = sorted(sim_scores,key=lambda x: x[1],reverse=True)
+	selected_course_indices = [i[0] for i in sim_scores[0:]]
+	selected_course_scores = [i[0] for i in sim_scores[0:]]
+
+	
+	result_df = df.iloc[selected_course_indices]
+	result_df['similarity_score'] = selected_course_scores
+	final_recommended_courses = result_df[['course_title','similarity_score','url','price','num_subscribers']]
+	return final_recommended_courses.head(num_of_rec)
+
+#CSS Template
+RESULT_TEMP = """
+<div style="width:90%;height:100%;margin:1px;padding:5px;position:relative;border-radius:5px;border-bottom-right-radius: 60px;
+box-shadow:0 0 15px 5px #ccc; background-color: white;
+  border-left: 5px solid #6c6c6c;">
+<h4>{}</h4>
+
+<p style="color:blue;"><span style="color:black;"> 🔗 </span><a href="{}",target="_blank">Link</a></p>
+<p style="color:blue;"><span style="color:black;">Price: </span>{}</p>
+<p style="color:blue;"><span style="color:black;">👨🏽‍🎓 Students: </span>{}</p>
+
+</div>
+"""
+
+# Search For Course 
+@st.cache
+def search_term_if_not_found(term,df):
+	result_df = df[df['course_title'].str.contains(term)]
+	return result_df
+
+
+def main():
+
+	st.markdown("<h1 style='text-align: center; color: black;'>Recommendation System</h1>", unsafe_allow_html=True)
+	#st.title("Recommendation System")
+
+	menu = ["Home","Recommend","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	df = load_data("data/dataset.csv")
+
+	if choice == "Home":
+		st.subheader("Home")
+		st.dataframe(df.head(10))
+
+
+	elif choice == "Recommend":
+		st.subheader("Recommend Products")
+		cosine_sim_mat = vectorize_text_to_cosine_mat(df['course_title'])
+		raw_search_term=st.text_input("Search")
+		search_term = raw_search_term.title()
+		num_of_rec = st.sidebar.number_input("Number",3,9,5)
+		if st.button("Recommend"):
+			if search_term is not None:
+				try:
+					results = get_recommendation(search_term,cosine_sim_mat,df,num_of_rec)
+					#   with st.beta_expander("Results as JSON"):
+					#	results_json = results.to_dict('index')
+					#	st.write(results_json)
+
+					for row in results.iterrows():
+						rec_title = row[1][0]
+						rec_score = row[1][1]
+						rec_url = row[1][2]
+						rec_price = row[1][3]
+						rec_num_sub = row[1][4]
+
+						# st.write("Title",rec_title,)
+						stc.html(RESULT_TEMP.format(rec_title,rec_url,rec_price,rec_num_sub),height=350)
+				except:
+					#results= "Not Found"
+					#st.warning(results)
+					st.info("Suggested Options include")
+					result_df = search_term_if_not_found(search_term,df)
+					st.dataframe(result_df)
+
+			
+	else:
+		st.subheader("About")
+		st.text("Product Recommendation System")
+		st.text("Web App built using streamlit")
+
+
+if __name__ == '__main__':
+	main()
+
+
+#Ultimate Investment Banking Course
